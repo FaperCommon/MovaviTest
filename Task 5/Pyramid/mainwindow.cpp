2fee0b4f6731ddb0bfe4a19167cad8e508da9bf7
FaperCommon <54195773+FaperCommon@users.noreply.github.com>
2020-02-06 13:15:52 +0700

    Added ability to switch between layers

--- Task 5/Pyramid/mainwindow.cpp
+++ Task 5/Pyramid/mainwindow.cpp
@@ -20,33 +20,33 @@ void MainWindow::on_OpenBut_clicked()
 {
     QString path = QFileDialog::getOpenFileName(this,  tr("Open image"), "", tr("*.png *.jpg"));
     if(path != ""){
-       scene = new QGraphicsScene();
-       QPixmap pic(path);
-       BuildPyramid(path);
-       //set_image(pic);
+       pic.load(path);
+       set_image(BuildPyramid(pic, ui->CB_layer_num->currentIndex()));
     }
 
 
 }
 
 void MainWindow::set_image(QPixmap pic){
-   // scene = new QGraphicsScene();
+    ui->Label_size->setText(QString::number(pic.size().width()) + 'x' + QString::number(pic.size().height()));
+    pic = pic.scaled(pic_size, Qt::KeepAspectRatioByExpanding);
+    scene = new QGraphicsScene();
     scene->addPixmap(pic);
     ui->PicView->setScene(scene);
 }
 
 
-void MainWindow::BuildPyramid(QString path){
-
-            QPixmap pic(path);                                   //size for compression
-            int koef_com = 2;
-            // QSize size = pic.size();
-             do{
-                  set_image(pic);
-                  pic = pic.scaled(pic.size()/=koef_com, Qt::KeepAspectRatioByExpanding);
-             }while(pic.size().width() > 2 && pic.size().height() > 2);
-
+QPixmap MainWindow::BuildPyramid(QPixmap pic, int pow){
+            int koef_com = 2;                       
+            pic_size = pic.size();
+             for(int i = 0;i < pow; i++)
+                  pic = pic.scaled((pic.size()/=koef_com), Qt::KeepAspectRatioByExpanding);
+    return pic;
 }
 
 
-
+void MainWindow::on_CB_layer_num_activated(int index)
+{
+    if(!pic.isNull())
+        set_image(BuildPyramid(pic,index));
+}
--- Task 5/Pyramid/mainwindow.h
+++ Task 5/Pyramid/mainwindow.h
@@ -19,13 +19,15 @@ public:
 
 private slots:
     void on_OpenBut_clicked();
+    void on_CB_layer_num_activated(int index);
 
 private:
     QGraphicsScene *scene;
     Ui::MainWindow *ui;
-    void set_image(QString path);
+    QPixmap pic;
+    QSize pic_size;
     void set_image(QPixmap pic);
-    void BuildPyramid(QString path);
+    QPixmap BuildPyramid(QPixmap pic,int pow);
 };
 
 #endif // MAINWINDOW_H
